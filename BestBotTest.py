025178244a12649e6bdbd5a162019de48a36d087
AskarSyundyukov <syundyukovaskar@gmail.com>
2020-01-27 22:59:41 +0300

    BestBotTest

--- BestBotTest.py
+++ BestBotTest.py
@@ -1,6 +1,5 @@
 import telebot
 import config
-import random
 import pyowm
 from telebot import types
 
@@ -13,10 +12,9 @@ def welcome(message):
     bot.send_sticker(message.chat.id, sti)
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Рандомное число")
-    item2 = types.KeyboardButton("Какая погода сейчас?")
+    item1 = types.KeyboardButton("Какая погода сейчас?")
 
-    markup.add(item1, item2)
+    markup.add(item1)
 
     bot.send_message(message.chat.id,
                      "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>".format(
@@ -25,39 +23,37 @@ def welcome(message):
 
 
 @bot.message_handler(content_types=['text'])
-def lalala(message):
+def conclusion(message):
     if message.chat.type == 'private':
-        if message.text == 'Рандомное число':
-            bot.send_message(message.chat.id, str(random.randint(0, 100)))
-        elif message.text == 'Как дела?':
+        if message.text == 'Какая погода сейчас?':
 
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
-            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+            markup = types.InlineKeyboardMarkup(row_width=3)
+            item1 = types.InlineKeyboardButton("Санкт-Петербург", callback_data='peter')
+            item2 = types.InlineKeyboardButton("Другой город", callback_data='other_city')
 
             markup.add(item1, item2)
 
-            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
-
-        elif message.text == 'Какая погода сейчас?':
-            city = bot.send_message(message.chat.id, "В каком городе вывести погоду?")
-            bot.register_next_step_handler(city, weather)
+            bot.send_message(message.chat.id, 'В каком городе вывести погоду?', reply_markup=markup)
 
         else:
             bot.send_message(message.chat.id, 'Error')
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
+def callback(call):
     try:
         if call.message:
-            if call.data == 'good':
-                bot.send_message(call.message.chat.id, 'Вот и отличненько')
-            elif call.data == 'bad':
-                bot.send_message(call.message.chat.id, 'Бывает')
+            if call.data == 'other_city':
+                city = bot.send_message(call.message.chat.id, 'Введите город')
+                bot.register_next_step_handler(city, weather)
+            elif call.data == 'peter':
+                bot.send_message(call.message.chat.id, 'Санкт Петербург')
+                city = 'Санкт Петербург'
+                bot.register_next_step_handler(city, weather)
+                
 
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?",
-                                  reply_markup=None)
+            # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?",
+            #                       reply_markup=None)
 
     except Exception as e:
         print(repr(e))
@@ -65,7 +61,7 @@ def callback_inline(call):
 
 def weather(message):
     try:
-        owm = pyowm.OWM(config.TOKEN_WEATHER)
+        owm = pyowm.OWM(config.TOKEN_WEATHER, language='ru')
         city = message.text
         weather = owm.weather_at_place(city)
         w = weather.get_weather()
@@ -73,8 +69,10 @@ def weather(message):
         wind = w.get_wind()["speed"]
         hum = w.get_humidity()
         desc = w.get_detailed_status()
-        bot.send_message(message.chat.id, "Сейчас в городе " + str(city) + " \n" + str(desc) + ", \nТемпература: " + str(
-            temperature) + "°C, \nВлажность: " + str(hum) + "%, \nСкорость ветра: " + str(wind) + "м/с.")
+        bot.send_message(message.chat.id,
+                         "Сейчас в городе " + str(city) + " \n" + str(desc) + ", \nТемпература: " + str(
+                             temperature) + "°C, \nВлажность: " + str(hum) + "%, \nСкорость ветра: " + str(
+                             wind) + "м/с.")
 
 
     except pyowm.exceptions.api_response_error.NotFoundError:
