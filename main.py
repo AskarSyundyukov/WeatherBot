da9ae2acf4c1461bb8a8839a84ac6d1ef743961a
radmirsafin <rmisc@yandex.ru>
2020-01-31 21:23:51 +0300

    initial version

---
+++ main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import messages
+from weather.WeatherReceiver import WeatherReceiver
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+)
+
+LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
+
+
+def handle_start_command(update, context):
+    logging.info(f"/start command received from "
+                 f"{update.effective_user.full_name} ({update.effective_chat.id})")
+    update.message.reply_text(messages.START_MESSAGE)
+
+
+def handle_help_command(update, context):
+    logging.info(f"/help command received from "
+                 f"{update.effective_user.full_name} ({update.effective_chat.id})")
+    update.message.reply_text(messages.HELP_MESSAGE)
+
+
+def handle_weather_command(update, context):
+    logging.info(f"/weather command received from "
+                 f"{update.effective_user.full_name} ({update.effective_chat.id})")
+    global weather_receiver
+    weather_info = weather_receiver.get_weather_by_city(messages.DEFAULT_CITY)
+    update.message.reply_text(messages.WEATHER_INFO_MESSAGE.format(
+        city=weather_info.city,
+        temperature=weather_info.temperature,
+        humidity=weather_info.humidity,
+        wind_speed=weather_info.wind_speed,
+        description=weather_info.description,
+    ))
+
+
+def handle_error(update, context):
+    logging.warning(f"Update {update} caused error {context.error}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Weather bot')
+    parser.add_argument('--bot_token', type=str, help='Telegram token', required=True)
+    parser.add_argument('--weather_token', type=str, help='Weather API token', required=True)
+    parser.add_argument('--debug', default=False, help='Enable debug', required=False)
+    args = parser.parse_args()
+
+    if args.debug:
+        LOG_LEVEL = logging.DEBUG
+    else:
+        LOG_LEVEL = logging.INFO
+
+    logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+
+    weather_receiver = WeatherReceiver(args.weather_token)
+
+    updater = Updater(args.bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', handle_start_command))
+    dispatcher.add_handler(CommandHandler('help', handle_help_command))
+    dispatcher.add_handler(CommandHandler('weather', handle_weather_command))
+    dispatcher.add_error_handler(handle_error)
+
+    updater.start_polling()
+    updater.idle()
---
+++ weather/WeatherReceiver.py
@@ -0,0 +1,31 @@
+import logging
+from pyowm import OWM
+from pyowm.exceptions.api_response_error import NotFoundError
+
+
+class WeatherInfo:
+    def __init__(self, **kwargs):
+        self.city = kwargs.get("city")
+        self.temperature = kwargs.get("temperature")
+        self.humidity = kwargs.get("humidity")
+        self.wind_speed = kwargs.get("wind_speed")
+        self.description = kwargs.get("description")
+
+
+class WeatherReceiver:
+    def __init__(self, weather_token):
+        self.owm = OWM(weather_token, language='ru')
+
+    def get_weather_by_city(self, city):
+        try:
+            received_weather = self.owm.weather_at_place(city).get_weather()
+        except NotFoundError as exc:
+            logging.warning(f"Cannot get weather for {city} city. Failed with exception: {exc}")
+        else:
+            return WeatherInfo(
+                city=city,
+                temperature=received_weather.get_temperature("celsius")["temp"],
+                wind_speed=received_weather.get_wind()["speed"],
+                humidity=received_weather.get_humidity(),
+                description=received_weather.get_detailed_status(),
+            )
